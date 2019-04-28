a99729fecbd90955ce38c814df9b9bf30fe5a56a
Samuel Elbaz <raregamer@hotmail.com>
2019-04-28 06:47:03 -0700

    Flower pseudo code

---
+++ src/main/FlowerGame.java
@@ -0,0 +1,29 @@
+package main;
+
+public class FlowerGame implements Games{
+	
+/*	flower game
+	pick flower 
+	has 6 leaves with random values -10 to +10
+	6 chances to take it or leave the flower.
+	---------------------
+	
+	make flower object, can be an array with 6 random values
+	(pick leave) go through each value and present to player to accept or not accept
+	if accept return value and add to points.
+	*/
+	
+	
+	
+	private int playerHealth;
+	
+	//Constructor
+	 	public FlowerGame(int playerHealth) {
+		this.playerHealth = playerHealth;
+	}
+	 	
+	 	public int startGame(int playerHealth) {
+	 		
+	 	}
+
+}
